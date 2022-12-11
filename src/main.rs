f56fe73ffe9c016c752e7715f92e01befbc62e96
justanotherbyte <wishymovies@gmail.com>
2022-12-11 22:42:40 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+#![allow(non_snake_case)]
+
+mod pages;
+
+use dioxus::{
+    prelude::*,
+    router::{Route, Router}
+};
+
+use pages::{HomePage, BlogPage};
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx!{
+        Router {
+            Route {
+                to: "/",
+                HomePage {}
+            },
+            Route {
+                to: "/blog",
+                BlogPage {}
+            }
+        }
+    })
+}
+
+fn main() {
+    dioxus::web::launch(app);
+}
+
+pub fn Navbar(cx: Scope) -> Element {
+    cx.render(rsx!{
+
+        nav {
+            class: "px-2 sm:px-4 py-2.5 rounded bg-transparent",
+            div {
+                class: "container flex flex-wrap items-center justify-between mx-auto",
+                a {
+                    class: "flex items-center",
+                    href: "/",
+                    h2 {
+                        class: "self-center text-xl font-semibold whitespace-nowrap text-white",
+                        "Viswa"
+                    }
+                },
+                ul {
+                    class: "flex flex-col py-1 px-3 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent bg-rose-400",
+                    li {
+                        a {
+                            class: "block pl-3 pr-4 rounded md:p-0 text-white md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent",
+                            "Home"
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
---
+++ src/pages.rs
@@ -0,0 +1,309 @@
+use std::borrow::Cow;
+
+use dioxus::prelude::*;
+use chrono::prelude::*;
+use gloo_net::http::Request;
+use serde::Deserialize;
+use dioxus_markdown::Markdown;
+
+pub fn HomePage(cx: Scope) -> Element {
+    let now = chrono::Utc::now();
+    let birthday = chrono::Utc.with_ymd_and_hms(2007, 03, 23, 7, 0, 0).unwrap();
+    let years_since = now.years_since(birthday).unwrap();
+    cx.render(rsx!{
+        section {
+            class: "bg-transparent",
+            div {
+                class: "grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12",
+                div {
+                    class: "mr-auto place-self-center lg:col-span-7",
+                    h1 {
+                        class: "max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white",
+                        "Hi. I'm Viswa"
+                    }
+                    p {
+                        class: "max-w-2xl mb-6 font-light text-zinc-500 lg:mb-8 md:text-lg lg:text-xl dark:text-zinc-400",
+                        p {
+                            "Nice to meet you! I'm currently a student in England. I love working on projects related to",
+                            a { 
+                              class: "text-rose-400 hover:animate-pulse",
+                              href: "https://en.wikipedia.org/wiki/Machine_learning",
+                              " Machine Learning"
+                            },
+                            ", "
+                            a {
+                                class: "text-sky-400 hover:animate-pulse",
+                                href: "https://en.wikipedia.org/wiki/Mathematics",
+                                " Mathematics"
+                            },
+                            " and my video game ",
+                            a {
+                                class: "text-emerald-400 hover:animate-pulse",
+                                href: "https://github.com/justanotherbyte/nova",
+                                "Nova"
+                            },
+                            ", which you should totally try out by the way 😉",
+                        }
+                    }
+                    a {
+                        href: "#about",
+                        class: "inline-flex items-center justify-center py-3 mr-3 text-base font-medium text-center text-white rounded-lg hover:text-rose-400 duration-200",
+                        "About"
+                    },
+                    a {
+                        href: "/blog",
+                        class: "inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg hover:text-rose-400 duration-200",
+                        "Blog"
+                    },
+                    a {
+                        href: "https://github.com/justanotherbyte",
+                        class: "inline-flex items-center justify-center py-3 mr-3 text-base font-medium text-center text-white rounded-lg hover:text-rose-400 duration-200",
+                        "GitHub"
+                    },
+                }
+                div {
+                    class: "hidden lg:mt-0 lg:col-span-5 lg:flex",
+                    img {
+                        src: "/avatar.svg"
+                    }
+                }
+            }
+        }
+
+        section {
+            class: "bg-transparent",
+            div {
+                class: "gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6",
+                div {
+                    class: "font-light text-zinc-500 sm:text-lg dark:text-zinc-400",
+                    h2 {
+                        class: "mb-4 text-4xl tracking-tight font-extrabold text-zinc-800 dark:text-white",
+                        "About Me"
+                    }
+                    p {
+                        class: "mb-4",
+                        "I'm a {years_since} year old currently studying in England. I have a huge interest in Machine Learning, Mathematics and Computer Science as a whole."
+                    }
+                    p {
+                        "I'll be beginning my journey on the ",
+                        a {
+                            class: "text-sky-400 hover:animate-pulse",
+                            href: "https://ibo.org/programmes/diploma-programme/",
+                            "International Baccalaureate Diploma "
+                        }
+                        "programme soon (I may already be on it, I've just forgotten to update this page). I'd like to study Computer Science at Cambridge or Imperial in the future :D"
+                    }
+                }
+                div {
+                    class: "grid grid-cols-2 gap-4 mt-8",
+                    img {
+                        class: "w-full rounded-lg",
+                        src: "/images/img328.jpg"
+                    }
+                    img {
+                        class: "mt-4 w-full lg:mt-10 rounded-lg",
+                        src: "/images/img321.jpg"
+                    }
+                }
+            }
+        }
+    })
+}
+
+#[inline_props]
+pub fn BlogPostCard(cx: Scope, title: String, content: String, image: String, slug: String) -> Element {
+    cx.render(rsx!{
+        div {
+            class: "max-w-sm bg-white rounded-lg shadow-md bg-zinc-800 p-3 mb-3",
+            a {
+                class: "",
+                img {
+                    class: "rounded-lg",
+                    src: "{image}"
+                }
+            }
+            div {
+                class: "p-3 mt-3",
+                a {
+                    h5 {
+                        class: "mb-2 text-2xl font-bold tracking-tight text-zinc-800 dark:text-white",
+                        "{title}"
+                    }
+                }
+                p {
+                    class: "mb-3 font-normal text-zinc-700 dark:text-zinc-400",
+                    "{content}"
+                }
+                a {
+                    href: "/blog?slug={slug}",
+                    class: "inline-flex items-center text-sm font-medium text-center text-sky-400 hover:text-rose-400 duration-200",
+                    "Read more ➜"
+                }
+            }
+        }
+    })
+}
+
+#[derive(Deserialize)]
+pub struct BlogPostModel {
+    pub slug: String,
+    pub content: String,
+    pub name: String,
+    pub description: String,
+    pub image: String,
+    pub created_at: String,
+    pub category: String
+}
+
+impl BlogPostModel {
+    fn loading() -> Self {
+        BlogPostModel { 
+            slug: "".into(),
+            content: "".into(),
+            name: "Loading...".into(),
+            description: "Hang on tight! Just warming up our gears here!".into(),
+            created_at: "0000-00-0".into(),
+            image: "/images/loading-transparent.png".into(),
+            category: "Loading...".into()
+         }
+    }
+}
+
+fn fetch_posts(cx: &Scope, state: &UseState<Vec<BlogPostModel>>) {
+    let state = state.to_owned();
+    cx.spawn(async move {
+        let resp = Request::new("https://blogserver-quiktea.vercel.app/posts")
+            .send()
+            .await
+            .unwrap()
+            .json::<Vec<BlogPostModel>>()
+            .await
+            .unwrap();
+
+        state.set(resp);
+    });
+}
+
+fn fetch_post(cx: &Scope<BlogPostPageProps>, state: &UseState<BlogPostModel>, slug: String) {
+    let state = state.to_owned();
+    let url = format!("https://blogserver-quiktea.vercel.app/post/{}", slug);
+    cx.spawn(async move {
+        let resp = Request::new(url.as_str())
+            .send()
+            .await
+            .unwrap()
+            .json::<BlogPostModel>()
+            .await
+            .unwrap();
+
+        state.set(resp);
+    });
+}
+
+pub fn BlogPage(cx: Scope) -> Element {
+    let window = web_sys::window().unwrap();
+    let location = window.location();
+    let current_url = location.to_locale_string().as_string().unwrap();
+    let url_parsed = url::Url::parse(current_url.as_str()).unwrap();
+    let query_params = url_parsed.query_pairs();
+    for (param, value) in query_params {
+        if param == Cow::from("slug") {
+            return cx.render(rsx!{
+                BlogPostPage { slug: value.to_string() }
+            })
+        }
+    }
+
+    let post_state: &UseState<Vec<BlogPostModel>> = use_state(&cx, || vec![BlogPostModel::loading()]);
+    cx.use_hook(|_| fetch_posts(&cx, &post_state));
+
+    let posts_parsed = post_state.get();
+
+    let mut counter = 0;
+
+    cx.render(rsx!{
+        section {
+            class: "bg-transparent",
+            posts_parsed.iter().rev().map(|model| {
+                if counter == 0 {
+                    counter += 1;
+                    rsx!{
+                        div {
+                            class: "grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12",
+                            div {
+                                class: "mr-auto place-self-center lg:col-span-7",
+                                h1 {
+                                    class: "max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white",
+                                    "{model.name}"
+                                }
+                                p {
+                                    class: "max-w-2xl mb-6 font-light text-zinc-500 lg:mb-8 md:text-lg lg:text-xl dark:text-zinc-400",
+                                    p {
+                                        "{model.description}"
+                                    }
+                                }
+                                a {
+                                    href: "/blog?slug={model.slug}",
+                                    class: "inline-flex items-center justify-center py-3 mr-3 text-base font-medium text-center text-sky-400 rounded-lg hover:text-rose-400 duration-200",
+                                    "Read More ➜"
+                                },
+                            }
+                            div {
+                                class: "hidden lg:mt-0 lg:col-span-5 lg:flex",
+                                img {
+                                    class: "rounded-lg",
+                                    src: "{model.image}"
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    rsx! { p{} }
+                }
+            })
+        }
+
+        div {
+            class: "container mx-auto grid grid-cols-3",
+            posts_parsed.iter().map(|model| rsx!{
+                BlogPostCard { title: model.name.clone(), content: model.description.clone(), image: model.image.clone(), slug: model.slug.clone() }
+            })
+        }
+    
+    })
+}
+
+#[inline_props]
+fn BlogPostPage(cx: Scope, slug: String) -> Element {
+    let post_state = use_state(&cx, BlogPostModel::loading);
+    cx.use_hook(|_| fetch_post(&cx, post_state, slug.to_owned()));
+
+    let post = post_state.get();
+
+    cx.render(rsx!{
+        section {
+            class: "container mx-auto mt-10",
+            p {
+                class: "text-zinc-400 text-center mb-3",
+                "{post.category}"
+            }
+            h2 {
+                class: "text-4xl text-white font-extrabold mb-3 text-center",
+                "{post.name}"
+            }
+            h6 {
+                class: "text-xl font-medium text-zinc-400 text-center mb-3",
+                "By Viswa Marepalli"
+            }
+            img {
+                class: "w-3/4 mx-auto rounded-lg h-96 object-cover object-bottom",
+                src: "{post.image}"
+            }
+            Markdown {
+                class: "text-zinc-300 mx-auto text-left mt-14 leading-relaxed max-w-5xl",
+                content: post.content.as_str()
+            }
+        }
+    })
+}
+
